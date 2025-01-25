57608dc4a3f40a57d9d8e58d58e51e929f7412c0
chadnitalukder2 <chadnitalukder2@gmail.com>
2025-01-25 21:47:32 +0600

    wip:: details page

--- src/page/ProjectDetails.vue
+++ src/page/ProjectDetails.vue
@@ -1,159 +1,201 @@
 <script setup>
 
+
 </script>
+
 <template>
-    <div class="container">
-   <div class="header">
-    Portfolio Project
-   </div>
-   <div class="content">
-    <div class="left">
-     <div class="image-slider">
-      <img alt="Artistic illustration of an elephant" height="400" src="https://storage.googleapis.com/a1aa/image/oLYJuz1zrmJBCp7NHLGoopPlA6PREGeH0EecRbr3H3LgJaIUA.jpg" width="800"/>
-     </div>
-     <div class="video">
-      <iframe allow="autoplay; fullscreen" allowfullscreen="" frameborder="0" src="https://player.vimeo.com/video/123456789">
-      </iframe>
-     </div>
+    <div class="project_details">
+        <div class="header">
+            <h1> Portfolio Project</h1>
+        </div>
+        <div class="content">
+            <div class="left">
+                <div class="image-slider">
+                    <img src="../assets/img/portfolioOne.png">
+                </div>
+
+            </div>
+            <div class="right">
+                <div class="description">
+                    <h3 class="title"> Description</h3>
+                    <p class="name"> <i class="fas fa-user"> </i>Chadni Talukder</p>
+                    <p class="name"><i style="font-size:18px" class="fa">&#xf0ac;</i>
+                        <a href="http://www.project-site.com">www.project-site.com</a>
+                    </p>
+                    <p class="name"> <i class="fas fa-calendar-alt"></i>
+                        25 December, 2016
+                    </p>
+                    <p description_describe>
+                        Aliquam euismod aliquam massa, quis eleifend dui sodales vitae. Interdum et malesuada fames ac
+                        ante ipsum primis in faucibus.
+                    </p>
+                </div>
+                <div class="technology">
+                    <h3 class="title">Technology</h3>
+                    <div class="tags">
+                        <p> HTML5 </p>
+                        <p> CSS3</p>
+                        <p>jQuery</p>
+                        <p> Ajax</p>
+                        <p> PHP5 </p>
+                    </div>
+
+                </div>
+                <div class="share">
+                    <h3 class="title"> Share </h3>
+                    <a href="#">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fab fa-google-plus-g"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="right">
-     <div class="description">
-      <h3>
-       Description
-      </h3>
-      <p>
-       <i class="fas fa-user">
-       </i>
-       Alex Smith
-      </p>
-      <p>
-       <i class="fas fa-link">
-       </i>
-       <a href="http://www.project-site.com">
-        www.project-site.com
-       </a>
-      </p>
-      <p>
-       <i class="fas fa-calendar-alt">
-       </i>
-       25 December, 2016
-      </p>
-      <p>
-       Aliquam euismod aliquam massa, quis eleifend dui sodales vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-      </p>
-     </div>
-     <div class="technology">
-      <h3>
-       Technology
-      </h3>
-      <span>
-       HTML5
-      </span>
-      <span>
-       CSS3
-      </span>
-      <span>
-       jQuery
-      </span>
-      <span>
-       Ajax
-      </span>
-      <span>
-       PHP5
-      </span>
-     </div>
-     <div class="share">
-      <h3>
-       Share
-      </h3>
-      <a href="#">
-       <i class="fab fa-facebook-f">
-       </i>
-      </a>
-      <a href="#">
-       <i class="fab fa-twitter">
-       </i>
-      </a>
-      <a href="#">
-       <i class="fab fa-google-plus-g">
-       </i>
-      </a>
-     </div>
-    </div>
-   </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
-        .header {
-            font-size: 24px;
-            font-weight: bold;
+.project_details {
+    padding: 50px 100px;
+    font-family: 'Archivo Black', sans-serif;
+
+    .header {
+        h1 {
+            font-size: 30px;
+            font-weight: 600;
+            color: var(--vt-c-black);
             margin-bottom: 20px;
         }
-        .content {
-            display: flex;
-            flex-wrap: wrap;
-        }
+
+    }
+
+    .content {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+
         .left {
-            flex: 2;
+           flex-basis: 55%;
             margin-right: 20px;
-        }
-        .right {
-            flex: 1;
-        }
-        .image-slider img {
-            width: 100%;
-            height: auto;
-        }
-        .description, .technology, .share {
-            margin-bottom: 20px;
-        }
-        .description h3, .technology h3, .share h3 {
-            font-size: 18px;
-            margin-bottom: 10px;
-        }
-        .description p, .technology p, .share p {
-            margin: 5px 0;
-        }
-        .technology span, .share a {
-            display: inline-block;
-            background: #e0e0e0;
-            padding: 5px 10px;
-            margin: 5px;
-            border-radius: 3px;
-            text-decoration: none;
-            color: #333;
-        }
-        .share a {
-            background: none;
-            color: #333;
-        }
-        .share a:hover {
-            color: #000;
-        }
-        .video {
-            position: relative;
-            padding-bottom: 56.25%;
-            height: 0;
-            overflow: hidden;
-            max-width: 100%;
-            background: #000;
-        }
-        .video iframe {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-        }
-        @media (max-width: 768px) {
-            .content {
-                flex-direction: column;
-            }
-            .left {
-                margin-right: 0;
-                margin-bottom: 20px;
+
+            .image-slider img {
+                width: 100%;
+                height: auto;
             }
         }
 
+        .right {
+            flex-basis: 40%;
+        }
+
+    }
+}
+
+.description,
+.technology,
+.share {
+    margin-bottom: 20px;
+}
+
+.title {
+    font-size: 20px;
+    margin-bottom: 20px;
+    font-weight: 600;
+    color: var(--vt-c-black);
+    position: relative;
+
+    &::after {
+        content: '';
+        display: block;
+        width: 100%;
+        height: 2px;
+        margin-top: -1px;
+        background-color: var(--vt-c-black);
+    }
+
+}
+
+.description {
+    .name {
+        i {
+            color: #000;
+            padding-right: 15px;
+            font-size: 16px;
+        }
+
+        color: #333;
+        font-size: 16px;
+
+        a {
+            color: #0099CC;
+            text-decoration: none;
+        }
+    }
+
+    .description_describe {
+        color: #666;
+        font-size: 14px;
+        padding-top: 15px;
+    }
+}
+
+.technology {
+    .tags {
+        display: flex;
+        gap: 10px;
+        flex-wrap: wrap;
+
+        p {
+            display: inline-block;
+            background: var(--vt-c-black);
+            padding: 5px 10px;
+            border-radius: 3px;
+            text-decoration: none;
+            color: var(--vt-c-white);
+        }
+    }
+
+
+}
+
+.share {
+    a {
+        text-decoration: none;
+        color: var(--vt-c-black);
+        margin-right: 10px;
+        font-size: 20px;
+        border: 2px solid var(--vt-c-black);
+        padding: 1px 8px;
+        border-radius: 5px;
+        display: inline-block;
+        transition: all 0.3s ease-in-out
+    }
+
+    a:hover {
+        color: var(--vt-c-white);
+        background: var(--vt-c-black);
+    }
+}
+
+.description p,
+.technology p,
+.share p {
+    margin: 5px 0;
+}
+
+
+@media (max-width: 768px) {
+    .content {
+        flex-direction: column;
+    }
+
+    .left {
+        margin-right: 0;
+        margin-bottom: 20px;
+    }
+}
 </style>
